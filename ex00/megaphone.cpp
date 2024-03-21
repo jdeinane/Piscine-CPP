e81366cd5221fccb5aaa1b12759063ec7ce77890
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2024-03-21 22:19:39 +0100

    ok c bon

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,26 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 09:21:51 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/03/19 09:21:55 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/03/21 22:19:26 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; ++i)
+		{
+			for (int j = 0; av[i][j]; ++j)
+				std::cout << (char)std::toupper(av[i][j]);
+			if (i < ac - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
