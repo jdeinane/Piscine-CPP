a2bfdc0f596d7a700aee640f7e66e6902922165b
Jennifer UBALDO <jubaldo@bess-f3r2s4.clusters.42paris.fr>
2024-03-25 20:56:18 +0100

    destroy changed

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:56:48 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/03/25 17:35:46 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/03/25 20:50:06 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie::Zombie(std::string name) : _name(name) {
 }
 
 Zombie::~Zombie() {
-	std::cout << _name << " is being killed" << std::endl;
+	std::cout << _name << " has been killed" << std::endl;
 }
 
 void	Zombie::announce() const {
