a9c04eca06a9e83ac088a3a5023a729460871bc6
Jennifer UBALDO <jubaldo@bess-f3r3s3.clusters.42paris.fr>
2024-03-26 16:11:59 +0100

    ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,32 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/26 14:07:18 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/03/26 16:07:52 by jubaldo          ###   ########.fr       */
+/*   Created: 2024/03/26 16:08:46 by jubaldo           #+#    #+#             */
+/*   Updated: 2024/03/26 16:08:47 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-#include "Weapon.hpp"
-
-int	main() {
-	{
-		Weapon	club = Weapon("crude spiked club");
-
-		HumanA	bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon	club = Weapon("crude spiked club");
-
-		HumanB	jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
-	return 0;
-}
