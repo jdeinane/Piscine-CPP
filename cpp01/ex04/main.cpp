4457887cc8270c5a7f24700882d755f0f410a381
Jennifer UBALDO <jubaldo@bess-f2r3s4.clusters.42paris.fr>
2024-04-09 17:34:35 +0200

    ex04 enfin

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,60 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 16:08:46 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/03/26 16:08:47 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/04/09 17:32:23 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::string	replaceAll(const std::string& source, const std::string& s1, const std::string& s2)
+{
+	std::string result;
+	std::size_t pos = 0;
+	std::size_t prevPos = 0;
+
+	while ((pos = source.find(s1, pos)) != std::string::npos) {
+		result += source.substr(prevPos, pos - prevPos) + s2;
+		pos += s1.length();
+		prevPos = pos;
+	}
+	result += source.substr(prevPos);
+	return result;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 4) {
+		std::cerr << "Wrong arguments" << std::endl;
+		return 1;
+	}
+	
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+
+	std::ifstream inputFile(filename.c_str());
+	if (!inputFile.is_open()) {
+		std::cerr << "Could not open file " << filename << std::endl;
+		return 1;
+	}
+	
+	std::string outputFilename = filename + ".replace";
+	std::ofstream outputFile(outputFilename.c_str());
+	if (!outputFile.is_open()) {
+		std::cerr << "Could not create file " << outputFilename << std::endl;
+		inputFile.close();
+		return 1;
+	}
+
+	std::string line;
+	while (std::getline(inputFile, line)) {
+		outputFile << replaceAll(line, s1, s2) << std::endl;
+		}
+	inputFile.close();
+	outputFile.close();
+	
+	return 0;
+}
