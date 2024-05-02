b2cb3650aaa855f6ebd1b619a889f899e10e6a8e
Jennifer UBALDO <jubaldo@bess-f1r6s4.clusters.42paris.fr>
2024-05-02 20:47:27 +0200

    .cpp

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,8 +6,53 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 18:48:35 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/04/29 18:50:08 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/02 20:47:10 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string const &name) : name(name) {
+	std::cout << "ClapTrap " << this->name << " is born!" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "Clap Trap " << this->name << " is destroyed." << std::endl;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (this->energyPoints > 0 && this-> hitPoints > 0) {
+		std::cout << "ClapTrap " << this->name << " attacks" << target 
+					<< ", causing" << this->attackDamage << " points of damage!" << std::endl;
+		this->energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->name << " is out of energy or dead." << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->name << " takes " << amount << " points of damage!" << std::endl;
+	if (amount >= this->hitPoints) {
+		this->hitPoints = 0;
+		std::cout << "ClapTrap " << this->name << " has been destroyed!" << std::endl;
+	}
+	else
+		this->hitPoints -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->energyPoints > 0 && this->hitPoints > 0) {
+		this->hitPoints += amount;
+		std::cout << "ClapTrap " << this->name << " repairs itself, restoring "
+					<< amount << " hit points!" << std::endl;
+		this->energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->name << " is out of energy or dead. It's too late for reparations." << std::endl;
+}
+
+
+
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,13 +6,28 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 18:48:30 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/04/29 18:49:43 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/02 20:15:26 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <iostream>
 
+class ClapTrap {
+private:
+	std::string name;
+	unsigned int hitPoints{10};
+	unsigned int energyPoints{10};
+	unsigned int attackDamage{0};
+
+public:
+	ClapTrap(std::string const &name);
+	~ClapTrap();
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
 
 #endif
