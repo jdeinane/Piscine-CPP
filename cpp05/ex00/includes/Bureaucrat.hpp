735a6964a510f3f9a183b373749ed0f913123376
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2024-07-14 23:10:44 +0200

    init

---
+++ cpp05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 23:08:01 by jubaldo           #+#    #+#             */
+/*   Updated: 2024/07/14 23:10:29 by jubaldo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+
+
+};
+
+#endif
