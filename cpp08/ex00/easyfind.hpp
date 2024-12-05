b2b6f523389b458101a24c71d8b5ad5f352bd571
Jennifer UBALDO <jubaldo@made-f0Cr6s3.clusters.42paris.fr>
2024-12-05 16:04:15 +0100

    commentaires

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:07:43 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/12/05 15:51:04 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/12/05 16:02:36 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ template <typename T>
 void easyfind(T &container, int n) {
 	// T peut etre n'importe quel type de container (vector, map, list, deque..)
 	
+	// definition d'un iterateur pour T::iterator
+	// ex: std::vector<int>::iterator it
 	typename T::iterator it = find(container.begin(), container.end(), n);
 	
 	if (it != container.end()) {
