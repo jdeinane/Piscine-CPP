0998d4ae8c2458458c933a3ec328895d944dd2de
Jennifer UBALDO <jubaldo@made-f0Cr6s3.clusters.42paris.fr>
2024-12-05 15:52:05 +0100

    replaced it - container.begin() by distance function

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:07:43 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/10/14 11:39:01 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/12/05 15:51:04 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
+#include <iterator>
 
 template <typename T>
 void easyfind(T &container, int n) {
+	// T peut etre n'importe quel type de container (vector, map, list, deque..)
+	
 	typename T::iterator it = find(container.begin(), container.end(), n);
-	if (it != container.end()) 
+	
+	if (it != container.end()) {
 		std::cout << "The first occurence of " << n << " is at index: "
-			<< it - container.begin() << std::endl;
+					// revient a ecrire it - container.begin() mais cela ne 
+					// fonctionnerait pas pour les lists et les sets 
+					//(iterateurs bidirectionnels)
+			<< std::distance(container.begin(), it) << std::endl;
+	}
+	
 	else
 		throw std::runtime_error("Element not found");
 };
