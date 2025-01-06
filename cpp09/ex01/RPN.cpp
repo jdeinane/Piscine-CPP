31a6d2acb9af70e350f078d53221f997219d9115
Jennifer UBALDO <jubaldo@bess-f3r2s8.clusters.42paris.fr>
2025-01-06 17:46:07 +0100

    RPN OK

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:09:50 by jubaldo           #+#    #+#             */
-/*   Updated: 2025/01/06 16:26:29 by jubaldo          ###   ########.fr       */
+/*   Updated: 2025/01/06 17:42:54 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,47 @@ bool RPN::isOperator(const std::string &token) {
     return token == "+" || token == "-" || token == "*" || token == "/";
 }
 
-int RPN::applyOperation(int a, int b, const std::string &op)
+int RPN::applyOperation(int a, int b, const std::string &op) {
+    if (op == "+")
+        return a + b;
+    if (op == "-")
+        return a - b;
+    if (op == "*")
+        return a * b;
+    if (op == "/") {
+        if (b == 0)
+            throw std::runtime_error("Error: Divion by zero.");
+        return a / b;
+    }
+    throw std::invalid_argument("Error: Invalid operator.");
+}
+
+int RPN::evaluateRPN(const std::string &expression) {
+    std::istringstream iss(expression);
+    std::string token;
+
+    while (iss >> token) {
+        if (isOperator(token)) {
+            if (_stack.size() < 2)
+                throw std::runtime_error("Error: Invalid expression.");
+            int b = _stack.top(); _stack.pop();
+            int a = _stack.top(); _stack.pop();
+            int result = applyOperation(a, b, token);
+            _stack.push(result);
+        }
+        else {
+            try {
+                std::istringstream tokenStream(token);
+                int number;
+                if (!(tokenStream >> number))
+                    throw std::runtime_error("Error: Invalid token.");
+                _stack.push(number);
+            } catch (...) {
+                throw std::runtime_error("Error: Invalid token.");
+            }
+        }
+    }
+    if (_stack.size() != 1)
+        throw std::runtime_error("Error: Invalid expression.");
+    return _stack.top();
+}
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,8 +6,25 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:10:11 by jubaldo           #+#    #+#             */
-/*   Updated: 2025/01/06 16:10:46 by jubaldo          ###   ########.fr       */
+/*   Updated: 2025/01/06 17:35:25 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+int main(int ac, char **av) {
+    if (ac != 2) {
+        std::cerr << "Usage: " << av[0] << " <expression>" << std::endl;
+        return 1;
+    }
+    
+    try {
+        RPN rpn;
+        int result = rpn.evaluateRPN(av[1]);
+        std::cout << result << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
