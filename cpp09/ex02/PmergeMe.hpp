8aeee783aff32af5898ad8f12c832f34bbdd8ba6
jdeinane <jenniferdianeu@gmail.com>
2025-01-10 14:47:10 +0100

    includes

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 11:51:33 by jubaldo           #+#    #+#             */
-/*   Updated: 2025/01/08 11:52:48 by jubaldo          ###   ########.fr       */
+/*   Updated: 2025/01/10 14:46:42 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,27 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <deque>
+#include <sstream>
+#include <stdexcept>
+#include <chrono>
+#include <algorithm>
 
 class PmergeMe {
-	
+	private:
+	std::vector<int> _vector;
+	std::deque<int> _deque;
+
+	void parseInput(int ac, char **av);
+	void validateNumber(const std::string& number) const;
+
+	void mergeInsertSortVector(std::vector<int> &container);
+	void mergeInsertSortDeque(std::deque<int>& container);
+
+	public:
+	PmergeMe();
+	~PmergeMe();
 };
 
 #endif
